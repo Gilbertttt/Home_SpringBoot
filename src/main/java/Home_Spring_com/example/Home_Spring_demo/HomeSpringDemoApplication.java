027fc6bf0cfcc5e6ce65871e8a562d193f1dd10d
Gilbertt <umorengilbert17@gmail.com>
2023-08-31 23:45:41 +0100

    Java SpringBoot

---
+++ src/main/java/Home_Spring_com/example/Home_Spring_demo/HomeSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package Home_Spring_com.example.Home_Spring_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeSpringDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Home_Spring_com/example/Home_Spring_demo/HomeSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Home_Spring_com.example.Home_Spring_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
